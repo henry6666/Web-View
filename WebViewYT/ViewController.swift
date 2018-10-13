603d5a4845aa16134648397e7f80d3bb2020fb0f
Henry Aguinaga <aguinagahenry@gmail.com>
2018-10-13 09:29:41 -0500

    Initial Commit

---
+++ WebViewYT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewYT
+//
+//  Created by Henry Aguinaga on 2018-10-13.
+//  Copyright © 2018 Henry Aguinaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
